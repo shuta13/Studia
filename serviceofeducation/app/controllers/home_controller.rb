7d34c7ee278252b6d781c96268a18a1d0a31ff48
shuta13 <hollystarsun@gmail.com>
2018-09-02 23:29:16 +0900

    目標決定画面の実装中、jQuery対応

--- serviceofeducation/app/controllers/home_controller.rb
+++ serviceofeducation/app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def top
   end
+
+  def decision
+  end
 end
--- serviceofeducation/config/routes.rb
+++ serviceofeducation/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  get 'home/top'
+  get 'top' => 'home#top'
+  get 'decision' => 'home#decision'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
