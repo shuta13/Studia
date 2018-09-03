f93fb66bd4a3e6b1adaef867df5cf874a21ab64f
shuta13 <hollystarsun@gmail.com>
2018-09-04 04:34:08 +0900

    Issueに詳細あり

--- serviceofeducation/app/controllers/records_controller.rb
+++ serviceofeducation/app/controllers/records_controller.rb
@@ -2,8 +2,10 @@ class RecordsController < ApplicationController
   def recordimage
   end
   def recordpage
+  end
+  def recordcreate
     @record = Record.new(studypage: params[:pageNumber])
     @record.save
-    #redirect_to('records/')
+    redirect_to('/records/recordwell')
   end
 end
--- serviceofeducation/config/routes.rb
+++ serviceofeducation/config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   get 'records/recordimage' => 'records#recordimage'
   get 'records/recordpage' => 'records#recordpage'
+  get 'records/recordwell' => 'records#recordwell'
 
   get 'goals/goaldecision' => 'goals#goaldecision'
   get 'goals/goalconfirm' => 'goals#goalconfirm'
 
   get 'top' => 'home#top'
+  post 'records/recordcreate' => 'records#recordcreate'
   post 'goals/goalcreate' => 'goals#goalcreate'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
